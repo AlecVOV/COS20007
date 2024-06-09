9122dbf0222548e82a795a0cd3d49e23c5b3ec99
Hoang Triet Thong Le <lehoangtrietthong2102004@gmail.com>
2024-06-09 18:09:39 +0700

    change the W2 Folder

---
+++ W2 - Setting Up Splashkit/SplaskitDemo/2.1.3+4P.cs
@@ -0,0 +1,14 @@
+using System;
+using SplashKitSDK;
+
+namespace SplaskitDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Window("Test Window", 800, 600);
+            SplashKit.Delay(5000);
+        }
+    }
+}
