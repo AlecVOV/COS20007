33219d75b3362530f31490c1097faf67c13b88b6
Hoang Triet Thong Le <lehoangtrietthong2102004@gmail.com>
2024-06-14 01:20:11 +0700

    Add project to done week 5 of OOP

---
+++ W5 - ShapeDrawerEnhancedExtremeDeluxe/Drawing.cs
@@ -0,0 +1,147 @@
+using SplashKitSDK;
+using System.IO;
+using System.Reflection.PortableExecutable;
+
+namespace DrawingShape
+{
+    public class Drawing
+    {
+        private readonly List<Shape> _shapes;
+        private Color _background;
+        StreamWriter writer;
+        StreamReader reader;
+
+
+        public Drawing(Color background)
+        {
+            _shapes = new List<Shape>();
+            _background = background;
+        }
+
+        public Drawing() : this(Color.White)
+        {
+
+        }
+        public List<Shape> SelectedShapes
+        {
+            get
+            {
+                var result = new List<Shape>();
+
+                foreach (var s in _shapes)
+                {
+                    if (s.Selected)
+                    {
+                        result.Add(s);
+                    }
+                }
+                return result;
+            }
+        }
+
+        public int ShapeCount
+        {
+            get { return _shapes.Count(); }
+        }
+
+        public Color Background
+        {
+            get
+            {
+                return _background;
+            }
+
+            set
+            {
+                _background = value;
+            }
+        }
+
+        public void Draw()
+        {
+            SplashKit.ClearScreen(_background);
+            foreach (Shape shape in _shapes)
+            {
+                shape.Draw();
+            }
+        }
+
+        public void SelectedShapesAt(Point2D pt)
+        {
+            foreach (Shape s in _shapes)
+            {
+                if (s.IsAt(pt))
+                    s.Selected = !s.Selected;
+                else
+                    s.Selected = s.Selected;
+            }
+        }
+
+        public void AddShape(Shape shape)
+        {
+            _shapes.Add(shape);
+        }
+
+        public bool RemoveShape(Shape shape)
+        {
+            return _shapes.Remove(shape);
+        }
+
+        public void Save(string filename)
+        {
+            try
+            {
+                writer = new StreamWriter(filename);
+                writer.WriteColor(_background);
+                writer.WriteLine(ShapeCount);
+
+                foreach (Shape s in _shapes)
+                {
+                    s.SaveTo(writer);
+                }
+            }
+            finally
+            {
+                writer.Close();
+            }
+        }
+
+        public void Load(string filename)
+        {
+            try
+            {
+                reader = new StreamReader(filename);
+                Shape s;
+                string kind;
+                _background = reader.ReadColor();
+                int count = reader.ReadInteger();
+                _shapes.Clear();
+
+                for (int i = 0; i < count; i++)
+                {
+                    kind = reader.ReadLine();
+                    switch (kind)
+                    {
+                        case "Rectangle":
+                            s = new MyRectangle();
+                            break;
+                        case "Circle":
+                            s = new MyCircle();
+                            break;
+                        case "Line":
+                            s = new MyLine();
+                            break;
+                        default:
+                            throw new InvalidDataException("Error at shape: " + kind);
+                    }
+                    s.LoadFrom(reader);
+                    AddShape(s);
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ W5 - ShapeDrawerEnhancedExtremeDeluxe/MyRectangle.cs
@@ -0,0 +1,79 @@
+﻿using SplashKitSDK;
+
+namespace DrawingShape
+{
+    public class MyRectangle : Shape
+    {
+        private int _width;
+        private int _height;
+
+
+        public MyRectangle(Color color, float x, float y, int _width, int _height) : base(color)
+        {
+            X = x;
+            Y = y;
+            Width = _width;
+            Height = _height;
+        }
+        public MyRectangle() : this(Color.Green, 0, 0, 200, 100)
+        {
+        }
+
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+        }
+        public override void Draw()
+        {
+            if (Selected)
+            {
+                DrawOutLine();
+            }
+            SplashKit.FillRectangle(Color, X, Y, Width, Height);
+        }
+
+        public override void DrawOutLine()
+        {
+            SplashKit.DrawRectangle(Color.Black, X - 2, Y - 2, _width + 4, _height + 4);
+        }
+
+        public override bool IsAt(Point2D pt)
+        {
+            return SplashKit.PointInRectangle(pt, SplashKit.RectangleFrom(X, Y, Width, Height));
+        }
+
+        public override void SaveTo(StreamWriter writer)
+        {
+            writer.WriteLine("Rectangle");
+            base.SaveTo(writer);
+            writer.WriteLine(Width);
+            writer.WriteLine(Height);
+        }
+
+        public override void LoadFrom(StreamReader reader)
+        {
+            base.LoadFrom(reader);
+            Width = reader.ReadInteger();
+            Height = reader.ReadInteger();
+        }
+    }
+}
